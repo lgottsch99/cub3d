e65a93e196b4bebc48948b94c0e867ae84ad953a
Lillian Gottschalk <lgottsch@c1r7s4.42prague.com>
2025-04-27 16:43:46 +0200

    added basic project structure

---
+++ project/includes/cub3d.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgottsch <lgottsch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/27 16:17:07 by lgottsch          #+#    #+#             */
+/*   Updated: 2025/04/27 16:42:24 by lgottsch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+// LIBRARIES ----------------------------------
+
+# include "../lib/mlx_linux/mlx.h" //minilibx for graphics rendering
+# include "../lib/full_libft/full_libft.h"
+# include <math.h>
+# include <X11/keysym.h> //defines some keycodes for hooks, only needed on linux?
+
+//for event macros like KeyPress 
+//https://codebrowser.dev/kde/include/X11/X.h.html
+//#include <X.h> //needed on mac?
+
+// MACROS ----------------------------------
+
+// STRUCTS ----------------------------------
+
+// FUNCTIONS ----------------------------------
+
+
+
+#endif
